6e608a62b721bed2793570bca0b9153230ff43b3
中島浩二 <knaka0209@gmail.com>
2023-07-04 07:52:36 +0900

    up, 0704a6

--- src/routes/task_project/[id]/IndexRow.svelte
+++ src/routes/task_project/[id]/IndexRow.svelte
@@ -24,17 +24,17 @@ if(status === '2'){
 <div class="">
     <div class="row justify-content-center p-1 task_index_row">
       <div class={`task_card_box card shadow-lg mb-0 ${bg_status_color}`}>
-        <div class="card-body">
+        <div class="card-body p-2">
           <div class="d-flex flex-column flex-md-row">
             <div class="card_col_icon px-md-2 py-2 ">
               <i class="bi bi-clipboard"></i>
             </div>
-            <div class="card_col_body  p-md-2">
+            <div class="card_col_body  p-md-0">
               <a href={`/task_items/${id}`}><span class="task_title fs-5">{title}</span>
               </a>
+              <br />
               <a href={`/task_items/edit/${id}`}><span class="task_title mx-2">[Edit]</span>
               </a>
-              <br />
               <span>{date}, ID: {id}</span>
             </div>
           </div>

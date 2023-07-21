faba438788dd3b4fafb1344a49ac1917f9f8e251
中島浩二 <knaka0209@gmail.com>
2023-07-22 07:50:42 +0900

    up, 0722a3

--- src/routes/task_project/+page.svelte
+++ src/routes/task_project/+page.svelte
@@ -44,6 +44,10 @@ const clearSearch = async function() {
     startProc();
 //console.log(items);
 }
+//
+const nextPage = function(path: string) {
+    location.href = path
+}
 /**
 *
 * @param
@@ -58,36 +62,44 @@ const search = async function() {
 </script>
 
 <div class="container my-2">
-	<div class="row">
-		<div class="col-md-6"><h1>project</h1></div>
-		<div class="col-md-6 text-end">
-			<a href={`/task_project/create`} class="btn btn-primary">Create
-			</a>
-		</div>
-	</div>
-	<hr class="my-1" />
     <div class="row">
-      <div class="col-md-12 text-end">
+        <div class="col-md-6"><h1>project</h1></div>
+        <div class="col-md-6 text-end">
+            <a href={`/task_project/create`} class="btn btn-primary">Create
+            </a>
+        </div>
+    </div>
+    <hr class="my-1" />
+    <div class="row">
+    <div class="col-md-12 text-end">
         <button class="btn btn-sm btn-outline-primary" on:click={clearSearch}
         >Clear</button>
         <span class="search_key_wrap">
-          <input type="text" size="24" class="mx-2" name="searchKey" id="searchKey" 
-           placeholder="Title search">
+        <input type="text" size="24" class="mx-2" name="searchKey" id="searchKey" 
+        placeholder="Title search">
         </span>
         <button class="btn btn-sm btn-outline-primary" on:click={search}>Search</button>      
-      </div>
+    </div>
     </div>	
-	<hr class="my-2" />
-	{#each data.items as item}
-	<div>
-        <a href={`/task_project/${item.id}`} ><h3>{item.name}</h3>
-        </a>		
-		<span>ID : {item.id}</span>
-		<a href={`/task_project/edit/${item.id}`} class="btn btn-sm btn-outline-primary">Edit
-		</a>
-		<hr class="my-2" />
-	</div>
-	{/each}	
+    <hr class="my-2" />
+    {#each data.items as item}
+    <div class="row">
+        <div class="col-md-6">
+            <a href={`/task_project/${item.id}`} ><h3 class="my-1">{item.name}</h3>
+            </a>		
+            <span>ID : {item.id}</span>
+        </div>
+        <div class="col-md-6 text-center pt-3">
+            <a href={`/task_project/${item.id}`} class="btn btn-sm btn-outline-primary">Show
+            </a>
+            <a href={`/task_project/edit/${item.id}`} class="btn btn-sm btn-outline-primary">Edit
+            </a>
+            <a href="#" on:click={nextPage(`/task_project/gantt/${item.id}`)} class="btn btn-sm btn-outline-primary">Gantt
+            </a>
+        </div>
+        <hr class="my-2" />
+    </div>
+    {/each}	
 </div>
 <!-- 
 <a href={`/task_project/${item.id}`} class="btn btn-outline-primary">Show
